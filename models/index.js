619c7d42a13f6ebc6e21f8ed8b949181ff35035a
mac <shebah712@gmail.com>
2020-09-11 09:34:50 -0700

    [deployment] add connection to atlas

--- models/index.js
+++ models/index.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
+
 // mongo connection
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/trailfinderDB', {
+mongoose.connect(process.env.ATLAS_URI || process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
